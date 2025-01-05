b3a1dceaa714ea62510112098bdfec34fce63874
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2025-01-05 02:08:13 +0000

    [Start]kadai8b

--- app/models/book.rb
+++ app/models/book.rb
@@ -26,5 +26,6 @@ class Book < ApplicationRecord
     scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) } 
     scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) } 
 
+    scope :created_day, ->(n) {where(created_at: n.days.ago.all_day)}
 
 end
